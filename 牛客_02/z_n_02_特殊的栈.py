6ac1cd4057eba1d0bfcee6d1187f43a744b9ef22
Pysuper <1821764535@qq.com>
2021-03-23 10:31:15 +0800

    添加 数据结构

---
+++ 牛客_02/z_n_02_特殊的栈.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# FileName ：z_n_02_特殊的栈.py
+# Author   ：zheng xingtao
+# Date     ：2021/3/23 9:10
+
+
+"""
+实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
+【要求】
+1．pop、push、getMin操作的时间复杂度都是O(1)。
+2．设计的栈类型可以使用现成的栈结构。
+"""
+
+
+class MyStack:
+    """
+    使用两个栈：data、min
+    data和min同时压栈
+    data中进行正常的压栈操作
+    min中：
+        第一个值，min和data一样操作
+        后面的：
+            如果值>=min中的栈顶，则在min的栈中在加一个min的栈顶，赋值一个一样的
+            如果值< min中的栈顶，就在min中加入这个值
+    最后返回的时候，从两个栈中，同时返回
+    那么就能在min中直接获取到data中的最小值了
+    """
+
+    def __init__(self):
+        self.stack_data = []
+        self.stack_min = []
+
+    def push(self, num: int):
+        if len(self.stack_min) == 0:
+            # 如果栈为空，则直接添加一个值
+            self.stack_min.append(num)
+
+        elif num < self.stack_min[len(self.stack_min) - 1]:
+            # 如果新进来的值，比min栈中的栈顶还要小
+            self.stack_min.append(num)
+        else:
+            # min栈中的栈顶更小,重复压入这个栈顶
+            self.stack_min.append(self.stack_min[len(self.stack_min) - 1])
+
+        # data栈中直接添加这个数
+        self.stack_data.append(num)
+
+    def pop(self):
+        if len(self.stack_data) == 0:
+            raise IndexError
+        self.stack_min.pop()
+        return self.stack_data.pop()
+
+    def get_min(self):
+        if len(self.stack_min) == 0:
+            raise IndexError
+
+        # 返回min栈的栈顶
+        return self.stack_min[self.stack_min[len(self.stack_min) - 1]]
---
+++ 牛客_02/z_n_03_队列和栈(仅).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# FileName ：z_n_03_队列和栈(仅).py
+# Author   ：zheng xingtao
+# Date     ：2021/3/23 9:10
+
+
+"""
+如何仅用队列结构实现栈结构？
+如何仅用栈结构实现队列结构？
+"""
+
+
+class QueueStack:
+    """
+    使用两个队列data、heap
+    data队列记录数据
+    需要返回数据的时候：
+        把data队列中的其他数据放入heap队列中，只留最后一个数
+        把data队列中的值拿出来
+        再调换data队列和heap队列的引用
+    """
+
+    def __init__(self):
+        pass
+
+
+class StackQueue:
+    """
+    使用两个栈：push、pop
+    所有进来的数都放在push栈中，取数的时候都从pop栈中获取
+    push中放了进来的数之后，全部倒入pop栈中：
+        一次性必须全部倒完，否则pop栈获取数据的时候，获取的值不对
+        pop栈中有数据的时候，一定不能想pop栈中倒入数据，否则pop中数据的顺序就乱了
+    """
+
+    def __init__(self):
+        pass
